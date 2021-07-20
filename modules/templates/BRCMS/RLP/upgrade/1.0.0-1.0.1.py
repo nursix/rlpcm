a1881d80464ca83e74b501a1c8b6e31577262eeb
Dominic König <dominic@nursix.org>
2021-07-20 18:47:49 +0200

    Release RLPCM-1.0.1

---
+++ modules/templates/BRCMS/RLP/upgrade/1.0.0-1.0.1.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+#
+# Database upgrade script
+#
+# RLPCM Template Version 1.0.0 => 1.0.1
+#
+# Execute in web2py folder after code upgrade like:
+# python web2py.py -S eden -M -R applications/eden/modules/templates/BRCMS/RLP/upgrade/1.0.0-1.0.1.py
+#
+import sys
+
+#from gluon.storage import Storage
+#from gluon.tools import callback
+from s3 import S3Duplicate
+
+# Override auth (disables all permission checks)
+auth.override = True
+
+# Failed-flag
+failed = False
+
+# Info
+def info(msg):
+    sys.stderr.write("%s" % msg)
+def infoln(msg):
+    sys.stderr.write("%s\n" % msg)
+
+# Load models for tables
+#ftable = s3db.org_facility
+
+IMPORT_XSLT_FOLDER = os.path.join(request.folder, "static", "formats", "s3csv")
+TEMPLATE_FOLDER = os.path.join(request.folder, "modules", "templates", "BRCMS", "RLP")
+
+# -----------------------------------------------------------------------------
+# Deploy new CMS items
+#
+if not failed:
+    info("Deploy new CMS items")
+
+    # File and Stylesheet Paths
+    stylesheet = os.path.join(IMPORT_XSLT_FOLDER, "cms", "post.xsl")
+    filename = os.path.join(TEMPLATE_FOLDER, "cms_post.csv")
+
+    # Only import relevant CMS posts, do not update any existing ones
+    def cms_post_duplicate(item):
+        name = item.data.get("name")
+        if name in ("HomepageIntro",
+                    ):
+            S3Duplicate(noupdate=True)(item)
+        else:
+            item.skip = True
+
+    # Import, fail on any errors
+    try:
+        with open(filename, "r") as File:
+            resource = s3db.resource("cms_post")
+            resource.configure(deduplicate = cms_post_duplicate)
+            resource.import_xml(File,
+                                format = "csv",
+                                stylesheet = stylesheet,
+                                )
+    except:
+        infoln("...failed")
+        infoln(sys.exc_info()[1])
+        failed = True
+    else:
+        if resource.error:
+            infoln("...failed")
+            infoln(resource.error)
+            failed = True
+        else:
+            infoln("...done")
+
+# -----------------------------------------------------------------------------
+# Finishing up
+#
+if failed:
+    db.rollback()
+    infoln("UPGRADE FAILED - Action rolled back.")
+else:
+    db.commit()
+    infoln("UPGRADE SUCCESSFUL.")
